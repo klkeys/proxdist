d4136059723c5fc04dd134fb2e54a7034d288f88
Kevin L. Keys <klkeysb@gmail.com>
2018-06-19 14:43:35 -0700

    added CPM code, backed up in-progress update (not working yet)

--- keys_zhou_lange_2017/cpm/cpm.jl
+++ keys_zhou_lange_2017/cpm/cpm.jl
@@ -1,18 +1,30 @@
+using Convex
+
+# Use Mosek solver
+using Mosek
+#solver = MosekSolver(LOG = 0, MSK_DPAR_OPTIMIZER_MAX_TIME = 1.0)
+solver = MosekSolver(LOG = 0)
+set_default_solver(solver)
+
+## Use SCS solver
+#using SCS
+#solver = SCSSolver(verbose=0)
+#set_default_solver(solver)
+
 """
-    sphere_orthant_projection(y)
 This function projects the point y onto the intersection of the unit sphere and the nonnegative orthant.
 """
-function sphere_orthant_projection(y::Vector{T}) where {T <: AbstractFloat} 
+function sphere_orthant_projection(y::Vector{T}) where {T <: AbstractFloat}
    i = indmax(y)
-   if y[i] <= 0 
-      x    = zeros(y)
-      x[i] = one(T) 
+   if y[i] <= 0
+      x = zeros(y)
+      x[i] = one(T)
       return x
    else
       x = deepcopy(y)
       for j = 1:length(x)
          if x[j] < 0
-            x[j] = zero(T) 
+            x[j] = zero(T)
          end
       end
       return x / norm(x)
@@ -20,146 +32,181 @@ function sphere_orthant_projection(y::Vector{T}) where {T <: AbstractFloat}
 end
 
 """
-    jacobi_solve(M, y, ρ)
-
-This function solves the equation `(ρ I + M) * x = y` by Jacobi's method.
+This function solves the equation (rho I + M) * x = y by Jacobi's method.
 """
-function jacobi_solve(M::Matrix{T}, y::Vector{T}, ρ::T; max_iter::Int = 100) where {T <: AbstractFloat} 
-  x = zeros(length(y))
-  z = zeros(length(y))
-  for i = 1:max_iter
-    z = y - M*x
-    if norm(z - ρ*x) < 1e-11
-        break
-    end
-    x = z/ρ
-  end
-  return x
+function jacobi_solve(M::Matrix{T}, y::Vector{T}, rho::T) where {T <: AbstractFloat}
+    x = zeros(T, length(y))
+    z = zeros(T, length(y))
+    for i = 1:100
+        z = y - M*x
+        if norm(z - rho*x) < 1e-11 break end
+            x = z/rho
+        end
+    return x
 end
 
 """
-    copositivity(M)
-
-This function find the minimum point of the quadratic form 
-
-    0.5 * x^t * M * x
-
-over the set of unit vectors with nonnegative components.
-This version uses the distance squared penalty in the proximal distance method. 
+ This function find the minimum point of the quadratic form
+       0.5 x^t M x
+ over the set of unit vectors with nonnegative components.
+ This version uses the distance squared penalty in the proximal
+ distance method.
 """
 function copositivity(M::Matrix{T}) where {T <: AbstractFloat}
 
     # Initialize control constants.
-    ρ        = one(T) 
-    ρ_inc    = 1.2
-    ρ_max    = 2^22
+    rho = one(T)
+    rho_inc = convert(T, 6/5) 
+    rho_max = 2^22
     max_iter = 5000
-    iters    = 0
-    quiet    = true 
+    iters = 0
 
     # Find the eigen-decomposition of M.
     (d,V) = eig(M)
 
     # Initialize parameters.
-    n    = size(M,1)
-    x    = rand(n)
-    x    = x/norm(x)
-    x    = sphere_orthant_projection(x)
-    loss = dot(x, M*x) / 2
+    n = size(M, 1)
+    x = rand(n)
+    x = x / norm(x)
+    x = sphere_orthant_projection(x)
+    loss = dot(x, M * x) / 2
 
     # Enter the proximal distance iteration loop.
     for n = 1:max_iter
         iters = iters + 1
 
         # Project onto the constraint set.
-        px   = sphere_orthant_projection(x)  
-        dist = norm(x-px)
-        
-        if n < 10 || n % 100 == 0
-            quiet || println(n,"  &   ",loss,"   &  ",dist," & ",ρ)
-        end
+        px = sphere_orthant_projection(x)
+        dist = norm(x - px)
+
+        # if n < 10 || n%100 == 0
+        #    println(n,"  &   ",loss,"   &  ",dist," & ",rho)
+        # end
 
         # Calculate the proximal distance update.
-        u = V'*(ρ*px)
-        u = u./(d+ρ)
-        x = V*u
+        u = V' * (rho * px)
+        u = u ./ (d + rho)
+        x = V * u
 
         # Check for convergence.
-        next_loss = dot(x, M*x) / 2
-        if abs(next_loss-loss)/(abs(loss)+1) < 1e-7 && dist < 1e-5
+        next_loss = dot(x, M * x) / 2
+        if abs(next_loss - loss) / (abs(loss) + 1) < 1e-7 && dist < 1e-5
             break
         else
             loss = next_loss
-            if n % 10 == 0
-                ρ = min(ρ_inc*ρ,ρ_max)
+            if n%10 == 0
+                rho = min(rho_inc * rho, rho_max)
             end
         end
     end
-    return (iters,x)
+    return (iters, sphere_orthant_projection(x))
 end
 
 """
-    accelerated_copositivity(M)
-
-This function find the minimum point of the quadratic form 
-
-    0.5 * x^t * M * x
-
-over the set of unit vectors with nonnegative components.
-This version uses the distance squared penalty in the proximal distance method. 
+ This function find the minimum point of the quadratic form
+       0.5 x^t M x
+ over the set of unit vectors with nonnegative components.
+ This version uses the distance squared penalty in the proximal
+ distance method.
 """
 function accelerated_copositivity(M::Matrix{T}) where {T <: AbstractFloat}
 
     # Initialize control constants.
-    ρ        = zero(T) 
-    ρ_max    = 2^22
+    rho = one(T)
+    rho_max = 2^22
     max_iter = 5000
-    iters    = 0
-    quiet    = true
+    iters = 0
 
     # Find the eigen-decomposition of M.
-    (d,V) = eig(M)
+    (d, V) = eig(M)
 
     # Initialize parameters.
-    p = size(M,1)
+    p = size(M, 1)
     x = rand(p)
-    x = x/norm(x)
+    x = x / norm(x)
     x = sphere_orthant_projection(x)
     y = deepcopy(x)
     z = zeros(p)
-    loss = dot(x, M*x) / 2
+    loss = dot(x, M * x) / 2
 
     # Enter the proximal distance iteration loop.
     for n = 1:max_iter
         iters = iters + 1
-        z = y + ( (n - zero(T)) / (n + 2*zero(T)) )*(y - x)
+        z = y + ((n - one(T)) / (n + 2*one(T))) * (y - x)
         x = deepcopy(y)
 
         # Project onto the constraint set.
-        pz   = sphere_orthant_projection(z)  
-        dist = norm(z-pz)
-        if n <= 10 || n % 100 == 0
-            quiet || println(n,"  &   ",loss,"  & ",dist,"  &  ",ρ)
-        end
+        pz = sphere_orthant_projection(z)
+        dist = norm(z - pz)
+
+        # if n <= 10 || n%100 == 0
+        #    println(n,"  &   ",loss,"  & ",dist,"  &  ",rho)
+        # end
 
         # Calculate the proximal distance update.
-        u = V'*(ρ*pz)
-        u = u./(d+ρ)
-        y = V*u
+        u = V' * (rho * pz)
+        u = u ./ (d + rho)
+        y = V * u
 
         # Check for convergence.
-        next_loss = dot(y, M*y) / 2
-        if abs(next_loss-loss)/(abs(loss) + zero(T)) < 1e-7 && dist < 1e-4
+        next_loss = dot(y, M * y) / 2
+        if abs(next_loss - loss) / (abs(loss) + one(T)) < 1e-7 && dist < 1e-4
             break
         else
             loss = next_loss
         end
-        if n % 100 == 0 
-            ρ = min(5.0*ρ,ρ_max)
+
+        if n%100 == 0
+            rho = min(5*rho, rho_max)
             x = deepcopy(y)
         end
     end
-
-    return (iters,y)
+    return (iters, sphere_orthant_projection(y))
 end
 
+"""
+
+    CoPositivityBySDP(::AbstractArray{T}) -> {Bool}
+
+Check whether M is a copositive matrix by semidefinite programming. M is
+copositive if M = A + B, where A is psd and B has nonnegative entries. Note this
+is only a sufficient condition. The function returns `true` if such a
+decomposition is found and `false` if otherwise.
+"""
+function CoPositivityBySDP(M::Array{T}) where {T <: AbstractFloat}
+  n = size(M, 1)
+  A = Convex.Variable(n, n)
+  B = Convex.Variable(n, n)
+  constraints = (M == A + B)
+  constraints += (A in :SDP)
+  constraints += B >= 0.0
+  problem = satisfy(constraints)
+  try solve!(problem)
+  catch e
+    return "timeout"
+  end
+  if problem.status == :Optimal
+    return "feasible"
+  else
+    return "infeasible"
+  end
+end
+
+"""
+
+    horn_matrix(::Int) -> Array{T}
+
+Generate an `n`-by-`n` Horn matrix.
+"""
+function horn_matrix(T::Type, n::Int)
+  M = ones(T, n, n)
+  for j = 2:n
+    M[j, j-1] = -one(T)
+    M[j-1, j] = -one(T)
+  end
+  M[1, n] = -one(T)
+  M[n, 1] = -one(T)
+  return M
+end
+
+horn_matrix(n::Int) = horn_matrix(Float64, n)
---
+++ keys_zhou_lange_2017/cpm/test_cpm_new.jl
@@ -0,0 +1,161 @@
+include("cpm.jl")
+n = 1000
+M = randn(n,n)
+M = 0.5*(M+M')
+
+# Hall and Newman example
+# n = 5
+# M = ones(n,n)
+# for j = 2:n
+#    M[j,j-1] = -1.0
+#    M[j-1,j] = -1.0
+# end
+# M[1,5] = -1.0
+# M[5,1] = -1.0
+# for i = 1:n
+#    println(M[i,:])
+# end
+#
+#tic(); (iters,x) = copositivity(M); toc()
+#loss = 0.5*dot(x,M*x)
+#println("iters = ",iters," loss = ",loss," norm = ",norm(x)," min = ",minimum(x))
+#tic(); (iters,y) = accelerated_copositivity(M); toc()
+#loss = 0.5*dot(y,M*y)
+#println("iters = ",iters," loss = ",loss," norm = ",norm(y)," min = ",minimum(y))
+## tic(); (iters,z) = accelerated_copositivity2(M); toc()
+## loss = 0.5*dot(z,M*z)
+## println("iters = ",iters," loss = ",loss," norm = ",norm(z)," min = ",minimum(z))
+## println(" norm of difference = ",norm(x-y))
+
+using Convex
+
+# Use Mosek solver
+using Mosek
+#solver = MosekSolver(LOG = 0, MSK_DPAR_OPTIMIZER_MAX_TIME = 1.0)
+solver = MosekSolver(LOG = 0)
+set_default_solver(solver)
+
+## Use SCS solver
+#using SCS
+#solver = SCSSolver(verbose=0)
+#set_default_solver(solver)
+
+
+"""
+
+    CoPositivityBySDP(::AbstractArray{Float64}) -> {Bool}
+
+Check whether M is a copositive matrix by semidefinite programming. M is
+copositive if M = A + B, where A is psd and B has nonnegative entries. Note this
+is only a sufficient condition. The function returns `true` if such a
+decomposition is found and `false` if otherwise.
+"""
+function CoPositivityBySDP(M::Array{Float64})
+  n = size(M, 1)
+  A = Convex.Variable(n, n)
+  B = Convex.Variable(n, n)
+  constraints = (M == A + B)
+  constraints += (A in :SDP)
+  constraints += B >= 0.0
+  problem = satisfy(constraints)
+  try solve!(problem)
+  catch e
+    return "timeout"
+  end
+  if problem.status == :Optimal
+    return "feasible"
+  else
+    return "infeasible"
+  end
+end
+
+"""
+
+    horn_matrix(::Int) -> Array{Float64}
+
+Generate an `n`-by-`n` Horn matrix.
+"""
+function horn_matrix(n::Int)
+  M = ones(n, n)
+  for j = 2:n
+    M[j, j-1] = -1.0
+    M[j-1, j] = -1.0
+  end
+  M[1, n] = -1.0
+  M[n, 1] = -1.0
+  return M
+end
+
+# Horn matrix example
+srand(1234)
+n = 2 .^ (2:6)
+n = sort([n; n + 1])
+timing = zeros(length(n), 3)
+loss = zeros(length(n), 2)
+losssdp = Array(AbstractString, length(n))
+for i = 1:length(n)
+  M = horn_matrix(n[i])
+  # proximal distance
+  tic(); (iters, y) = copositivity(M); timing[i, 1] = toc();
+  loss[i, 1] = 0.5 * dot(y, M * y)
+  # accelerated proximal distance
+  tic(); (iters, y) = accelerated_copositivity(M); timing[i, 2] = toc();
+  loss[i, 2] = 0.5 * dot(y, M * y)
+  # semidefinite programming
+  tic(); losssdp[i] = CoPositivityBySDP(M); timing[i, 3] = toc();
+end
+
+println("\\begin{table}")
+println("\t\\centering")
+println("\t\\begin{tabular}{rrrrrrr}")
+println("\t\t\\multicolumn{1}{c}{Dimensions} & \\multicolumn{3}{c}{Optima} & \\multicolumn{3}{c}{CPU Times} \\\\ ")
+println("\t\t\\\cmidrule(r){2-4} \\cmidrule(r){5-7}")
+println("\t\t\$n\$ & PD & aPD & Mosek & PD & aPD & Mosek\\\\")
+println("\\hline")
+for k = 1:length(n)
+	@printf("\t\t%d & %3.6f & %3.6f & %s & %3.4f & %3.4f & %3.4f\\\\\n", n[k],
+  loss[k, 1], loss[k, 2], losssdp[k], timing[k, 1], timing[k, 2], timing[k, 3])
+end
+println("\t\t\\bottomrule")
+println("\t\\end{tabular}")
+println("\t\\caption{CPU times and optima for testing copositivity of Horn matrix. Here \$n\$ is the size of Horn matrix, PD is the proximal distance algorithm, aPD is the accelerated proximal distance algorithm, and Mosek is the Mosek solver.}")
+println("\t\\label{tab:copos-horn}")
+println("\\end{table}")
+
+
+## Symmetric matrix example
+#srand(1234)
+#n = 2 .^ (2:8)
+#timing = zeros(length(n), 3)
+#loss = zeros(length(n), 2)
+#losssdp = Array{AbstractString}(length(n))
+#for i = 1:length(n)
+#  M = randn(n[i], n[i])
+#  M = 0.5 * (M + M')
+#  # proximal distance
+#  tic(); (iters, y) = copositivity(M); timing[i, 1] = toc();
+#  loss[i, 1] = 0.5 * dot(y, M * y)
+#  # accelerated proximal distance
+#  tic(); (iters, y) = accelerated_copositivity(M); timing[i, 2] = toc();
+#  loss[i, 2] = 0.5 * dot(y, M * y)
+#  # semidefinite programming
+#  tic(); losssdp[i] = CoPositivityBySDP(M); timing[i, 3] = toc();
+#end
+#
+#println("\\begin{table}")
+#println("\t\\centering")
+#println("\t\\begin{tabular}{rrrrrrr}")
+#println("\t\t\\multicolumn{1}{c}{Dimensions} & \\multicolumn{3}{c}{Optima} & \\multicolumn{3}{c}{CPU Times} \\\\ ")
+#println("\t\t\ \\cmidrule(r){2-4} \\cmidrule(r){5-7}")
+#println("\t\t\$n\$ & PD & aPD & Mosek & PD & aPD & Mosek\\\\")
+#println("\\hline")
+#for k = 1:length(n)
+#	@printf("\t\t%d & %3.6f & %3.6f & %s & %3.4f & %3.4f & %3.4f\\\\\n", n[k],
+#  loss[k, 1], loss[k, 2], losssdp[k],
+#  timing[k, 1], timing[k, 2], timing[k, 3])
+#end
+#println("\t\t\\bottomrule")
+#println("\t\\end{tabular}")
+#println("\t\\caption{CPU times and optima for testing copositivity of random symmetric matrix. Here \$n\$ is the size of matrix, PD is the proximal distance algorithm, aPD is the accelerated proximal distance algorithm, and Mosek is the Mosek solver.}")
+#println("\t\\label{tab:copos-sym}")
+#println("\\end{table}")
