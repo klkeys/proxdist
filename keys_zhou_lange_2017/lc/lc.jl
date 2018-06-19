4ee79c564ab0122ffeeb2ccd1a61c0de2362dfa8
Kevin L. Keys <klkeysb@gmail.com>
2018-06-19 15:39:19 -0700

    updated LC code to v0.6

--- keys_zhou_lange_2017/lc/lc.jl
+++ keys_zhou_lange_2017/lc/lc.jl
@@ -4,7 +4,7 @@
 This function projects the vectors `s` and `t` onto the set where `dot(s,t) = 0` and all components of `s` and `t` are nonnegative.
 This projection is of value in solving the linear complementarity problem.
 """
-function complementarity_projection(s::Vector{T}, t::Vector{T}) where {T} = Float64
+function complementarity_projection(s::Vector{T}, t::Vector{T}) where {T <: AbstractFloat}
     n     = length(s)
     (x,y) = (zeros(T, n), zeros(T, n))
 
@@ -28,13 +28,14 @@ end
 
 This function solves the linear complementarity problem by an accelerated proximal distance algorithm.
 """
-function linear_complementarity(A::Matrix{T}, b::Vector{T}) where {T} = Float64
+function linear_complementarity(A::Matrix{T}, b::Vector{T}) where {T <: AbstractFloat}
 
     # Initialize control constants.
     ρ        = one(T)
     ρ_max    = 2^22
     max_iter = 5000
     iters    = 0
+    quiet    = true
 
     # Initialize arrays and the loss.
     p = size(A, 1)
@@ -43,7 +44,7 @@ function linear_complementarity(A::Matrix{T}, b::Vector{T}) where {T} = Float64
     # Perform the projection.
     (px, py) = complementarity_projection(x, y)
     dist = vecnorm(x - px) + vecnorm(y - py)
-    loss = 0.5*vecnorm(y - A*x - b)^2
+    loss = (vecnorm(y - A*x - b)^2) / 2
 
     # Enter the proximal distance loop.
     for n = 1:max_iter
@@ -57,11 +58,11 @@ function linear_complementarity(A::Matrix{T}, b::Vector{T}) where {T} = Float64
         # Project onto the constraints.
         (px, py) = complementarity_projection(xb, yb)
         dist     = sqrt(vecnorm(xb - px)^2 + vecnorm(yb - py)^2)
-        loss     = 0.5*vecnorm(yb - A*xb - b)^2
-        obj      = loss + 0.5*ρ*dist^2
+        loss     = (vecnorm(yb - A*xb - b)^2) / 2
+        obj      = loss + (ρ*dist^2) / 2
 
         # Output the current iterate.
-        if n <= 10 || n % 100 == 0
+        if (n <= 10 || n % 100 == 0) && !quiet 
             println(n,"  &   ",loss,"  & ",dist,"  &  ",ρ)
         end
 
@@ -85,4 +86,43 @@ function linear_complementarity(A::Matrix{T}, b::Vector{T}) where {T} = Float64
     end
 
     return (iters, xa, ya)
+end
+
+"""
+This function solves the linear complementarity problem by an accelerated proximal distance algorithm.
+"""
+function linear_complementarity_milp(A::Matrix{T}, b::Vector{T}) where {T <: AbstractFloat}
+  n = length(b)
+
+  # use Gurobi solver
+  m = Model(solver = GurobiSolver(OutputFlag = 0))
+
+  # define variables
+  @variable(m, x[1:n] >= 0.0)
+  @variable(m, y[1:n] >= 0.0)
+
+  # equality constraint y = Ax + b
+  for i = 1:n
+    @constraint(m, y[i] == sum(A[i, j] * x[j] for j = 1:n) + b[i])
+  end
+
+  # complimentarity constraint
+  for i = 1:n
+    addSOS1(m, [x[i], y[i]])
+  end
+
+  # no objective function -> feasibility problem
+  @objective(m, Min, 0.0)
+
+  # ready, set, go!
+  #display(m)
+  status = solve(m)
+
+  # Return
+  xsol = zeros(n); ysol = zeros(n)
+  for i = 1:n
+    @inbounds xsol[i] = getvalue(x[i])
+    @inbounds ysol[i] = getvalue(y[i])
+  end
+  return xsol, ysol, m
 end
--- keys_zhou_lange_2017/lc/test_lc.jl
+++ keys_zhou_lange_2017/lc/test_lc.jl
@@ -1,7 +1,49 @@
-source("lc.jl")
+using JuMP
+using Gurobi
 
-p = 50
-A = randn(p , p)
-A = A'*A
-b = randn(p)
-(x, y) = linear_complementarity(A, b)
+include("lc.jl")
+
+srand(123)
+n = 2 .^ (2:6)
+timing = zeros(length(n), 2)
+loss = zeros(length(n), 2)
+
+# precompile the routines 
+i = 1
+A = randn(n[i], n[i])
+A = A' * A
+b = randn(n[i])
+tic(); (iters, x1, y1) = linear_complementarity(A, b); timing[i, 1] = toc();
+tic(); (iters, y) = (x2, y2) = linear_complementarity_milp(A, b);
+
+for i = 1:length(n)
+  A = randn(n[i], n[i])
+  A = A' * A
+  b = randn(n[i])
+
+  # proximal distance
+  tic(); (iters, x1, y1) = linear_complementarity(A, b); timing[i, 1] = toc();
+  loss[i, 1] = 0.5 * sum(abs2, y1 - A*x1 - b)
+
+  # mixed integer programming
+  tic(); (iters, y) = (x2, y2) = linear_complementarity_milp(A, b);
+  timing[i, 2] = toc();
+  loss[i, 2] = 0.5 * sum(abs2, y2 - A*x2 - b)
+end
+
+println("\\begin{table}")
+println("\t\\centering")
+println("\t\\begin{tabular}{rrrrr}")
+println("\t\t\\multicolumn{1}{c}{Dimensions} & \\multicolumn{2}{c}{Optima} & \\multicolumn{2}{c}{CPU Times} \\\\ ")
+println("\t\t\ \\cmidrule(r){2-3} \\cmidrule(r){4-5}")
+println("\t\t\$n\$ & PD & Gurobi & PD & Gurobi\\\\")
+println("\\hline")
+for k = 1:length(n)
+	@printf("\t\t%d & %3.6f & %3.6f & %3.4f & %3.4f\\\\\n", n[k],
+  loss[k, 1], loss[k, 2], timing[k, 1], timing[k, 2])
+end
+println("\t\t\\bottomrule")
+println("\t\\end{tabular}")
+println("\t\\caption{CPU times and optima for linear complementarity problem with randomly generated data. Here \$n\$ is the size of matrix, PD is the proximal distance algorithm, and Gurobi is the Gurobi solver.}")
+println("\t\\label{tab:lincomp}")
+println("\\end{table}")
